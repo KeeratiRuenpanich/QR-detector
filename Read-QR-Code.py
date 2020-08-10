d9a4d2a33d9218b91709e16aed9e1ccb65390dfa
Keerati_R <rug9988@gmail.com>
2020-08-10 10:33:14 +0700

    Initial Commit

---
+++ Read-QR-Code.py
@@ -0,0 +1,27 @@
+import cv2
+# initalize the cam
+cap = cv2.VideoCapture(0)
+# initialize the cv2 QRCode detector
+detector = cv2.QRCodeDetector()
+while True:
+    _, img = cap.read()
+    # detect and decode
+    data, bbox, straight_qrcode = detector.detectAndDecode(img)
+    # check if there is a QRCode in the image
+    if bbox is not None:
+        # display the image with lines
+        n_lines = len(bbox)
+        for i in range(n_lines):
+            # draw all lines
+            point1 = tuple(bbox[i][0])
+            point2 = tuple(bbox[(i+1) % n_lines][0])
+            cv2.line(img, point1, point2, color=(255, 0, 0), thickness=10)
+        if data:
+            print("[+] QR Code detected, data:", data)
+            print(bbox)
+    # display the result
+    cv2.imshow("img", img)
+    if cv2.waitKey(1) == ord("q"):
+        break
+cap.release()
+cv2.destroyAllWindows()
